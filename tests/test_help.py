12b9b3d4a2a07714d61734d5a93943ab8c10a5c2
Santo Cariotti <santo@dcariotti.me>
2021-07-16 21:22:33 +0200

    test: adds for nested buttons in help command

--- tests/test_help.py
+++ tests/test_help.py
@@ -8,42 +8,54 @@ from . import TIMEOUT, bot_tag
 
 @pytest.mark.asyncio
 async def test_help_buttons(client: TelegramClient):
-    """Tests all the md buttons in the help command
+    """Tests all the buttons in the help command
 
     Args:
         client (TelegramClient): client used to simulate the user
     """
+
     conv: Conversation
     async with client.conversation(bot_tag, timeout=TIMEOUT) as conv:
-        buttons = (
-            "md_esami_link",
-            "md_lezioni_link",
-            "md_professori",
-            "md_biblioteca",
-            "md_gruppi",
-            "md_cus",
-            "md_cloud",
-            "md_sdidattica",
-            "md_studenti",
-            "md_cea",
-            "md_ersu",
-            "md_ufficioersu",
-            "md_urp",
-            "md_drive",
-            "md_gitlab",
-            "md_opismanager",
-            "md_contributors",
-            "md_help",
-            "exit_cmd",
-        )
+        buttons = {
+            "sm_help_dip_cdl": [
+                "md_esami_link",
+                "sm_aulario",
+                "md_lezioni_link",
+                "md_professori",
+                "sm_rapp_menu",
+                "md_biblioteca",
+                "md_gruppi",
+            ],
+            "reg_button_home": [],  # tested by `/regolamentodidattico`
+            "sm_help_segr": ["md_sdidattica", "md_studenti", "md_cea"],
+            "sm_help_ersu": [
+                "md_ersu",
+                "md_ufficioersu",
+                "md_urp",
+            ],
+            "sm_help_misc": ["md_drive", "md_gitlab"],
+            "sm_help_projects_acknowledgements": [
+                "md_opismanager",
+                "md_contributors",
+            ],
+            "md_help": [],
+            "exit_cmd": [],
+        }
 
-        for button in buttons:
-            await conv.send_message("/help")  # send a command
-            resp: Message = await conv.get_response()
+        resp: Message
+        for first, nested in buttons.items():
+            for button in nested:
+                # Open the `help` menu
+                await conv.send_message("/help")
+                resp = await conv.get_response()
+                assert resp.text
 
-            assert resp.text
+                # Click the `first` button
+                await resp.click(data=first)
+                resp = await conv.get_edit()
+                assert resp.text
 
-            await resp.click(data=button)  # click the inline button
-            resp: Message = await conv.get_edit()
-
-            assert resp.text
+                # Click the nested element in the menu
+                await resp.click(data=button)
+                resp = await conv.get_edit()
+                assert resp.text
